7849343742ea66bb613d81480ce6bd9e15c24b32
GS <91503751+gaasol13@users.noreply.github.com>
2024-11-01 23:16:21 +0000

    updates

---
+++ src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateController.java
@@ -0,0 +1,31 @@
+package com.version1.finalprojectdashboard.FinalProjectDashboard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class CandidateController {
+
+		
+		private final CandidateService candidateService;
+		
+		@Autowired
+		public CandidateController(CandidateService candidateService) {
+			this.candidateService = candidateService;
+		}
+		
+		
+		// GetMapping > basic webpage (returns a message)
+		@GetMapping("/")
+		public String welcome() {
+			return "Welcome to the Candidate Dashboard";
+		}
+		
+
+}
+
+
+
