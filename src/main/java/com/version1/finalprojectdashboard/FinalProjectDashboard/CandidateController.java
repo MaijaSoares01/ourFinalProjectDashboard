ee83ec7062fac09f2b347f33635de514355610fa
GS <91503751+gaasol13@users.noreply.github.com>
2024-11-01 23:41:24 +0000

    Update CandidateController.java
    Adding for a new html

--- src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateController.java
+++ src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateController.java
@@ -1,6 +1,12 @@
 package com.version1.finalprojectdashboard.FinalProjectDashboard;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +30,21 @@ public class CandidateController {
 			return "Welcome to the Candidate Dashboard";
 		}
 		
+
+		@GetMapping("/all")
+		public String getAllCandidates(Model model) {
+			List<Candidate> candidates = candidateService.getAllCandidates();
+			model.addAttribute("Candidates", candidates);
+			return "candidates";
+		}
+		
+		//Adding a new candidate
+		@PostMapping("/add-candidate")
+		public String addCandidate(@ModelAttribute Candidate candidate) {
+			candidateService.addCandidate(candidate);
+			return "redirect:/candidates";
+		}
+
 
 }
 
