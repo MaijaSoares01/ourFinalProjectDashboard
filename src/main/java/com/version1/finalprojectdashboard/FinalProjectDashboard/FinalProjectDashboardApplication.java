c53e923f42ddcee53a98a80d02823eaac3c3ad98
MaijaSoares01 <maijasoares01@gmail.com>
2024-11-01 10:11:28 +0000

    Skeleton of dashboard project

---
+++ src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/FinalProjectDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.version1.finalprojectdashboard.FinalProjectDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/version1/finalprojectdashboard/FinalProjectDashboard/FinalProjectDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version1.finalprojectdashboard.FinalProjectDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
