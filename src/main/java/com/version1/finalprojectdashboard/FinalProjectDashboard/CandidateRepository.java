f87d9ffd630002241ea55e1a516146e7e782c10c
GS <91503751+gaasol13@users.noreply.github.com>
2024-11-02 18:33:03 +0000

    Update CandidateRepository.java
    Sorry gang, i encountered an issue with this class, i only included the string save, but somehow i could not merge it into candidate repository

--- src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateRepository.java
+++ src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateRepository.java
@@ -1,11 +1,15 @@
 package com.version1.finalprojectdashboard.FinalProjectDashboard;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
-	
+
+	String save(Optional<Candidate> update);
+
 	
 
 }
