73213d071247a529d9f79bc96e3690bc3d4255bf
GS <91503751+gaasol13@users.noreply.github.com>
2024-11-05 23:55:46 +0000

    Update
    Filter for the candidate

--- src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateRepository.java
+++ src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 import java.util.Optional;
 
-
+import com.version1.finalprojectdashboard.FinalProjectDashboard.Candidate;
 import com.version1.finalprojectdashboard.FinalProjectDashboard.enums.Gender;
 import com.version1.finalprojectdashboard.FinalProjectDashboard.enums.VisaStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,12 +21,17 @@ import com.version1.finalprojectdashboard.FinalProjectDashboard.enums.RejectionS
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
 
 
-	String save(Optional<Candidate> update);
-
-	List<Candidate> findByInterviewScoreGreaterThanEqual(Integer score);
-
-	List<Candidate> findByRejectionStage(RejectionStage rejectionStage);
-
+	   //find candidates by candidate name containing a keyword
+    List<Candidate> findByCandidateNameContainingIgnoreCase(String keyword);
+    List<Candidate> findByLocationContainingIgnoreCase(String keyword);
+    List<Candidate> findByLocationContaining(String location);
+    List<Candidate> findByGender(String gender);
+    List<Candidate> findByVisaStatus(String visaStatus);
+    List<Candidate> findByProfile(String profile);
+    List<Candidate> findByEducation(String education);
+    List<Candidate> findByDegree(String degree);
+    List<Candidate> findByRejectionStage(String rejectionStage);
+    List<Candidate> findByRoleApplied_JobRoleName(String roleApplied);
 
 	// Custom search method using JPQL
 	@Query("SELECT c FROM Candidate c WHERE " +
