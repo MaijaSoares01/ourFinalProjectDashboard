23f64aa3ba50523bef9fdf4481914dd90b9ccb20
SolaresG <gaasol.13@proton.me>
2024-11-07 13:04:27 +0000

    Candidates done

--- src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateService.java
+++ src/main/java/com/version1/finalprojectdashboard/FinalProjectDashboard/CandidateService.java
@@ -1,6 +1,7 @@
 package com.version1.finalprojectdashboard.FinalProjectDashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -31,69 +32,10 @@ public class CandidateService{
 
 
 	// Search and filter candidates based on multiple criteria
-	public List<Candidate> searchCandidates(String keyword, String gender, String visaStatus, String profile,
-											String rejectionStage, String education, String degree, String roleApplied) {
-//		// Start with all candidates
-//		List<Candidate> candidates = candidateRepository.findAll();
+	public List<Candidate> searchCandidates(String keyword, String gender, String visaStatus,
+											String profile, String rejectionStage,
+											String education, String degree, String roleApplied) {
 //
-//		// Apply keyword filter (name or location)
-//		if (keyword != null && !keyword.isEmpty()) {
-//			String lowerKeyword = keyword.toLowerCase();
-//			candidates = candidates.stream()
-//					.filter(c -> c.getCandidateName().toLowerCase().contains(lowerKeyword) ||
-//							c.getLocation().toLowerCase().contains(lowerKeyword))
-//					.collect(Collectors.toList());
-//		}
-//
-//		// Filter by gender
-//		if (gender != null && !gender.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getGender().equals(gender))
-//					.collect(Collectors.toList());
-//		}
-//
-//		//fiilter by visa status
-//		if (visaStatus != null && !visaStatus.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getVisaStatus().equals(visaStatus))
-//					.collect(Collectors.toList());
-//		}
-//
-//		//filter by profile
-//		if (profile != null && !profile.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getProfile().equals(profile))
-//					.collect(Collectors.toList());
-//		}
-//
-//		// Filter by rejection stage
-//		if (rejectionStage != null && !rejectionStage.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getRejectionStage().equals(rejectionStage))
-//					.collect(Collectors.toList());
-//		}
-//
-//		// Filter by education
-//		if (education != null && !education.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getEducation().equals(education))
-//					.collect(Collectors.toList());
-//		}
-//
-//		// Filter by degree
-//		if (degree != null && !degree.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getDegree().equals(degree))
-//					.collect(Collectors.toList());
-//		}
-//
-//		// Filter by role applied
-//		if (roleApplied != null && !roleApplied.isEmpty()) {
-//			candidates = candidates.stream()
-//					.filter(c -> c.getRoleApplied() != null
-//							&& c.getRoleApplied().getJobRoleName().equals(roleApplied))
-//					.collect(Collectors.toList());
-//		}
 		return  candidateRepository.searchCandidates(keyword, gender, visaStatus, profile, rejectionStage, education, degree, roleApplied);
 	}
 	public List<String> getDistinctGenders() {
